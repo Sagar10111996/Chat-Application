26bdc58feb705baa316cd4ed9638c7749552d0b5
Sagar10111996 <sagardhaware25@gmail.com>
2022-11-10 18:35:44 +0100

    CORS error fixed, now allowing request from all origins

--- backend/chat_backend/chat_backend/settings.py
+++ backend/chat_backend/chat_backend/settings.py
@@ -57,12 +57,16 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# allowing the requests from all origins
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'chat_backend.urls'
 
