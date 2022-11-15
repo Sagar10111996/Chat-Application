2914c48627b68c39d4dcbf6afbf4b89a75d87fd9
Sagar10111996 <sagardhaware25@gmail.com>
2022-11-15 13:56:39 +0100

    Login bug fix

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react'
+import jwt_decode from "jwt-decode";
 
-
-import Counter from './components/counter';
 import Login from './pages/auth';
 import ChatScreen from './pages/chat';
+import { Constants } from './constants/api';
+import { setLoggedInUserDetails, setLoginStatus } from './redux/slice/user'
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    let accessToken = localStorage.getItem(Constants.STORAGE_ITEM_ACCESS_TOKEN)
+    if (accessToken) {
+      let decoded = jwt_decode(accessToken);
+      dispatch(setLoggedInUserDetails({id: decoded.user_id, email: decoded.email, first_name: decoded.first_name, last_name: decoded.last_name}))
+      dispatch(setLoginStatus(true))
+    }
+  })
+
   const isUserLoggedIn = useSelector(state => state.user.isUserLoggedIn)
   return (
     <div className="App">
